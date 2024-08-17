d26c65596d78646ef9eaffce0a2214b00e6cd0f2
daeheekim <dae4805@naver.com>
2024-08-17 16:14:09 +0900

    update husky-project

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import path from 'path';
-import typescript from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import prettierConfig from 'eslint-config-prettier';
-import prettierPlugin from 'eslint-plugin-prettier';
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+const path = require('path');
+const typescript = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const prettierConfig = require('eslint-config-prettier');
+const prettierPlugin = require('eslint-plugin-prettier');
 
 const compat = new FlatCompat({
     baseDirectory: path.resolve('./'),
 });
 
-export default [
+module.exports = [
     js.configs.recommended,
     ...compat.extends('plugin:react/recommended'),
     ...compat.extends('plugin:@typescript-eslint/recommended'),
